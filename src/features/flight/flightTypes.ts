92208b459ea7a43b510aa24d7fc64fdbc719d346
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-01 09:21:06 -0800

    add type in flightTypes

---
+++ src/features/flight/flightTypes.ts
@@ -0,0 +1,13 @@
+export interface Flight{
+    flight_id: number;
+    airline: string;
+    origin: string;
+    destination: string;
+    departure_time: string[];
+    arrival_times: string[];
+}
+
+export interface FlightClass{
+    class: string;
+    price: number;
+}
